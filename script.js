25a1b3a9149042c8718948ff3a1c76fbf74437f9
Anudeep Ch <anudeep@Anudeeps-MacBook-Air.local>
2025-04-04 21:11:22 +0530

    gamelogic

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ const winCombs = [
     [0, 3, 6], [1, 4, 7], [2, 5, 8], 
     [0, 4, 8], [2, 4, 6]
 ];
+
+sbox.forEach(box => {
+    box.addEventListener("click", () => {
+        if (!gameActive || box.textContent !== "") return;
+    
+        let index = box.getAttribute("data-index");
+        boardState[index] = Playern;
+        box.textContent = Playern; // fixed this line
+        box.disabled = true;
+    
+        if (checkWinner()) {
+            statusText.textContent = `Player ${Playern} Wins! `;
+            gameActive = false;
+            return;
+        }
+    
+        if (!boardState.includes("")) {
+            statusText.textContent = "It's a Draw! ";
+            gameActive = false;
+            return;
+        }
+    
+        Playern = Playern === "X" ? "O" : "X";
+        statusText.textContent = `Player ${Playern}'s Turn`;
+    });
+});
